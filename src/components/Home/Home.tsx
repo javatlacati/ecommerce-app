b971373418b1dedaadf7d1192e00db7ba19c9dd3
rlopezcarro <rlopezcarro@deloitte.com>
2024-07-10 21:53:18 -0600

    se agregan estilos a la barra de navegación

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,27 +1,50 @@
 import React, {FC} from 'react';
 import {Link} from "react-router-dom";
+import styled from "styled-components";
 
 
 interface HomeProps {
 }
 
+export const NavbarContainer = styled.nav`
+    width: 100%;
+    height: 50px;
+    background-color: purple;
+    display: flex;
+    flex-direction: column;`
+
+const NavbarLinkContainer = styled.div`
+    display: flex;
+`
+
+export const NavbarLink = styled(Link)`
+    color: white;
+    font-size: x-large;
+    font-family: Arial, Helvetica, sans-serif;
+    text-decoration: none;
+    margin: 10px;
+
+    &:hover,
+    &:focus {
+        color: blue;
+    }
+
+    &:active {
+        color: rgba(56, 58, 54, 0.78);
+    }
+;`
+
 const Home: FC<HomeProps> = () => (
     //TODO Display a carousel or a grid of featured products.
     //TODO Style the homepage using CSS Modules or Styled Components.
     <div data-testid="Home">
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/cart">Cart</Link>
-                </li>
-                <li>
-                    <Link to="/products">Products</Link>
-                </li>
-            </ul>
-        </nav>
+        <NavbarContainer>
+            <NavbarLinkContainer>
+                <NavbarLink className="nav-link active" to="/">Home</NavbarLink>
+                <NavbarLink className="nav-link" to="/cart">Cart</NavbarLink>
+                <NavbarLink className="nav-link" to="/products">Products</NavbarLink>
+            </NavbarLinkContainer>
+        </NavbarContainer>
     </div>
 );
 
