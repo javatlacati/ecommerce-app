f184d4ed963762dd387bfc5c1474c0cd02d3bff7
rlopezcarro <rlopezcarro@deloitte.com>
2024-07-12 22:28:28 -0600

    pruebas unitarias a las acciones del reducer del slice de carrito de compras usando el store real

--- src/redux/cartSlice.test.ts
+++ src/redux/cartSlice.test.ts
@@ -1,33 +1,56 @@
 import {cartSlice, CartSliceState} from "./cartSlice";
 import {useDispatch} from "react-redux";
+import {createStore} from "@reduxjs/toolkit";
 
 describe('Cart Slice', () => {
+
     test('it should handle adding an item to the cart', () => {
         // Arrange
         const initialState: CartSliceState = {items: [], totalToBePaid: 0};
         const daPayload = {productId: 1, quantity: 2, price: 4};
 
         const addToCart = cartSlice.actions.addItem;
-        const dispatch = useDispatch();
+        //const dispatch = useDispatch();
+
+        const realStore = createStore(cartSlice.reducer, initialState);
 
         // Act
-        dispatch(addToCart(daPayload));
+        realStore.dispatch(addToCart(daPayload));
 
         // Assert
-        expect(initialState).toEqual({items:[{productId: 1, quantity: 2}], totalToBePaid: 12});
+        expect(realStore.getState()).toEqual({items: [{productId: 1, quantity: 2}], totalToBePaid: 8});
     });
 
-    // test('it should handle removing an item from the cart', () => {
-    //     // Arrange
-    //     const initialState: CartSliceState = {items: [{productId:1,productQuantity:2, productPrice:4}], totalToBePaid: 12};
-    //     const dispatch = useDispatch();
-    //     const daPayload = {payload: {id: 1, quantity: 2}};
-    //     dispatch(cartSlice.actions.removeItem(daPayload));
-    //     if (initialState.items) {
-    //         expect(initialState.items.get('1')).toBe(undefined);
-    //     }
-    //
-    // });
+    test('it should handle removing an item from the cart', () => {
+        // Arrange
+        const initialState: CartSliceState = {
+            items: [{productId: 1, productQuantity: 2, productPrice: 4}],
+            totalToBePaid: 12
+        };
+        const daPayload = {productId: 1, price: 4};
+        const realStore = createStore(cartSlice.reducer, initialState);
 
+        // Act
+        realStore.dispatch(cartSlice.actions.removeItem(daPayload));
 
+        expect(realStore.getState()).toEqual({
+            items: [{productId: 1, productQuantity: 1, productPrice: 4}],
+            totalToBePaid: 4
+        });
+
+    });
+
+    test('it should handle clearing the cart', () => {
+        // Arrange
+        const initialState: CartSliceState = {
+            items: [{productId: 1, productQuantity: 2, productPrice: 4}],
+            totalToBePaid: 12
+        };
+        const realStore = createStore(cartSlice.reducer, initialState);
+
+        // Act
+        realStore.dispatch(cartSlice.actions.clearCart());
+
+        expect(realStore.getState()).toEqual({items: [], totalToBePaid: 0});
+    })
 })
